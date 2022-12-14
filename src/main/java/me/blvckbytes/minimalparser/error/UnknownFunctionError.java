3d7c0320a1dedb579f86b112685f3385d9c42cb6
Philip Hell <blvckbytes@gmail.com>
2022-12-14 02:51:04 +0100

    deleted unused errors

--- src/main/java/me/blvckbytes/minimalparser/error/UnknownFunctionError.java
+++
@@ -1,8 +0,0 @@
-package me.blvckbytes.minimalparser.error;
-
-public class UnknownFunctionError extends AParserError {
-
-  public UnknownFunctionError(int row, int col) {
-    super(row, col, "Unknown function encountered");
-  }
-}
--- src/main/java/me/blvckbytes/minimalparser/error/UnknownVariableError.java
+++
@@ -1,8 +0,0 @@
-package me.blvckbytes.minimalparser.error;
-
-public class UnknownVariableError extends AParserError {
-
-  public UnknownVariableError(int row, int col) {
-    super(row, col, "Unknown variable encountered");
-  }
-}
