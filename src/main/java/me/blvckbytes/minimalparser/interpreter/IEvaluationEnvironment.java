359e752c28fd8170c64f3fa1776d02a06e84669a
Philip Hell <blvckbytes@gmail.com>
2022-12-14 07:14:34 +0100

    renamed IEvaluationContext to IEvaluationEnvironment and moved it into the interpreter package

---
+++ src/main/java/me/blvckbytes/minimalparser/interpreter/IEvaluationEnvironment.java
@@ -0,0 +1,25 @@
+package me.blvckbytes.minimalparser.interpreter;
+
+import me.blvckbytes.minimalparser.functions.IExpressionFunction;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+public interface IEvaluationEnvironment {
+
+  /**
+   * Mapping identifiers to available functions which an expression may invoke
+   */
+  Map<String, IExpressionFunction> getFunctions();
+
+  /**
+   * Mapping identifiers to available live variables which an expression may resolve
+   */
+  Map<String, Supplier<Object>> getLiveVariables();
+
+  /**
+   * Mapping identifiers to available static variables which an expression may resolve
+   */
+  Map<String, Object> getStaticVariables();
+
+}
